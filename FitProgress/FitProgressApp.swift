9e2d9b34a229795c6915e5a84681cc5e47092693
Dani Castillo <castillo_m_d@outlook.es>
2025-02-07 11:53:44 +0100

    Initial Commit

---
+++ FitProgress/FitProgressApp.swift
@@ -0,0 +1,20 @@
+//
+//  FitProgressApp.swift
+//  FitProgress
+//
+//  Created by Daniel Castillo Montoya on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitProgressApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
